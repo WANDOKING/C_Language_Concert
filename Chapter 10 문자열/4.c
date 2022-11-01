b605ceadb812cbb57fc986df01954a7f3d43b8a2
Dowan Kim <108464645+WANDOKING@users.noreply.github.com>
2022-11-01 22:37:50 +0900

    Create 4.c

---
+++ Chapter 10 문자열/4.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAX 50 // max length of string
+
+int main(void)
+{
+    char input[MAX];
+    char temp[MAX];
+
+    printf("텍스트를 입력하시오: ");
+    fgets(input, sizeof(char) * MAX, stdin);
+
+    printf("공백이 제거된 텍스트: ");
+
+    int bReading = TRUE;
+    int i = 0;
+    while (input[i] != '\0')
+    {
+        if (bReading == FALSE && input[i] == ' ')
+        {
+            i++;
+            continue;
+        }
+
+        if (input[i] == ' ')
+        {
+            bReading = FALSE;
+        }
+        else
+        {
+            bReading = TRUE;
+        }
+
+        putchar(input[i]);
+        i++;
+    }
+
+    return 0;
+}
